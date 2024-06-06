d8af8eed31f35a4886695e7d8b4d72c627fb08c8
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-06-06 21:51:57 +0300

    Final operations were done on the MVC project to be used for testing.

---
+++ UnitTestApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,135 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitTestApp.Web.Models;
+using UnitTestApp.Web.Repositories;
+
+namespace UnitTestApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IRepository<Product> _repository;
+
+        public ProductsController(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _repository.GetAllAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var product = await _repository.GetByIdAsync((int)id);
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Stock,Color")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.CreateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var product = await _repository.GetByIdAsync((int)id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name,Price,Stock,Color")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _repository.Update(product);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _repository.GetByIdAsync((int)id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+
+            _repository.Delete(product);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            var product = _repository.GetByIdAsync(id).Result;
+
+            return product != null;
+        }
+    }
+}
---
+++ UnitTestApp.Web/Helpers/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestApp.Web.Helpers
+{
+    public class Helper
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
